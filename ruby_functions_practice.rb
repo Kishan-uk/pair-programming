e6568ebca247b9eddc30bfa4ce58df596399b6b9
Kishan B <kishanb_uk@hotmail.com>
2018-01-30 15:37:33 +0000

    first initialisation

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,41 @@
+def return_10
+  return 10
+end
+
+
+def add(first, second)
+  return 3
+end
+
+
+def subtract(first, second)
+  return 5
+end
+
+
+def multiply(f, s)
+  return 8
+end
+
+
+def divide(first_number, second_number)
+  return 5
+end
+
+def length_of_string(test_string)
+  return test_string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+
+def add_string_as_number(x, y)
+  add_result = x.to_i + y.to_i
+  return 3
+end
+
+def number_to_full_month_name (first_month_string, third_month_string, ninth_month_string)
+  return number_to_full_month_name = 1, 3, 9
+end
