b1ff3b42998355ff59f1435a3fd57738fa5fb452
Kishan B <kishanb_uk@hotmail.com>
2018-01-30 20:49:01 +0000

    function tasks completed

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -64,24 +64,25 @@ class Functions_Practice < MiniTest::Test
     assert_equal( "Sep", ninth_month_string )
   end
 
-  #
-  #
-  # #Further
-  #
-  # #Given the length of a side of a cube calculate the volume
-  # def test_volume_of_cube()
-  #   #add test code here
-  # end
-  #
-  # #Given the radius of a sphere calculate the volume
-  # def test_volume_of_sphere()
-  #   #add test code here
-  # end
-  #
-  # #Given a value in farenheit, convert this into celsius.
-  # def test_fahrenheit_to_celsius()
-  #   #add test code here
-  # end
+  #Further
+
+  #Given the length of a side of a cube calculate the volume
+  def test_volume_of_cube()
+    volume_result = volume_of_cube( 5 )
+    assert_equal( 125, volume_result )
+  end
+
+  #Given the radius of a sphere calculate the volume
+  def test_volume_of_sphere()
+    volume_result = volume_of_sphere( 5 )
+    assert_equal( 523.6, volume_result )
+  end
+
+  #Given a value in farenheit, convert this into celsius.
+  def test_fahrenheit_to_celsius()
+     temp_result = fahrenheit_to_celsius( 70 )
+     assert_equal(21, temp_result)
+  end
 
 
 end
